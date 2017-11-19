fcabfe4492bf289fd2a996a771d423ca0b21ddec
Rohan Mishra <rohanmishra2017@gmail.com>
2017-11-18 23:17:35 -0800

    Updated run.py

--- run.py
+++ run.py
@@ -3,3 +3,18 @@
 run.py
 """
 
+import click
+
+from sandbox import Sanbox
+
+@click.command()
+@clicl.command('init', default='', help='Creates the sandbox directory')
+def init(init):
+    """ Create a sandbox """
+    if init is None:
+        sandbox = Sandbox('')
+    else if init == 'LYFT':
+        sandbox = Sandbox('LYFT')
+    else:
+        sandbox = Sandbox('UBER')
+    
